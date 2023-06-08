ec9a4caa0fad49582166aee25190f0d3150f6097
Fernando Rocha <fernando.rocha@im.ufal.br>
2023-06-08 19:31:16 -0300

    feat: implementando metodos

--- code/src/components/BestTransport.vue
+++ code/src/components/BestTransport.vue
@@ -6,14 +6,10 @@
       </b-navbar-brand>
     </b-navbar>
   </div>
-
 </template>
 
 <script>
-import {
-  BNavbar,
-  BNavbarBrand,
-} from 'bootstrap-vue'
+import { BNavbar, BNavbarBrand } from "bootstrap-vue";
 
 export default {
   components: {
@@ -21,24 +17,68 @@ export default {
     BNavbarBrand,
   },
   data() {
-    const appName = ''
+    const appName = "";
 
     return {
       appName,
-    }
+    };
   },
   created() {
     // Implemente aqui o GET dos dados da API REST
     // para que isso ocorra na inicialização da pagina
-    this.appName = 'Melhor Frete'
+    this.appName = "Melhor Frete";
   },
   methods: {
-    // Implemente aqui os metodos utilizados na pagina
-    methodFoo() {
-      console.log(this.appName)
+    analyzeFreight() {
+      fetch("http://localhost:3000/transport")
+        .then((response) => response.json())
+        .then((data) => {
+          const cotacoesFiltradas = data.filter((cotacao) => {
+            const pesoFrete =
+              this.weight > 100
+                ? "cost_transport_heavy"
+                : "cost_transport_light";
+            return (
+              cotacao.city.toLowerCase() === this.destination.toLowerCase() &&
+              cotacao[pesoFrete]
+            );
+          });
+
+          this.bestFreightEconomic = cotacoesFiltradas.reduce(
+            (anterior, atual) => {
+              const custoAnterior = parseFloat(
+                anterior[
+                  this.weight > 100
+                    ? "cost_transport_heavy"
+                    : "cost_transport_light"
+                ].replace("R$ ", "")
+              );
+              const custoAtual = parseFloat(
+                atual[
+                  this.weight > 100
+                    ? "cost_transport_heavy"
+                    : "cost_transport_light"
+                ].replace("R$ ", "")
+              );
+              return custoAtual < custoAnterior ? atual : anterior;
+            }
+          );
+
+          this.bestFreightFast = cotacoesFiltradas.reduce((anterior, atual) => {
+            const tempoAnterior = parseInt(anterior.lead_time.replace("h", ""));
+            const tempoAtual = parseInt(atual.lead_time.replace("h", ""));
+            return tempoAtual < tempoAnterior ? atual : anterior;
+          });
+        })
+        .catch((error) => {
+          console.error(
+            "Ocorreu um erro ao obter as cotações de frete:",
+            error
+          );
+        });
     },
   },
-}
+};
 </script>
 
 <style scoped>
