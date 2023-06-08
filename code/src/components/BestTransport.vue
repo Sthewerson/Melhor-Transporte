95a16f41eefcd9eb53f521cf2241e9c92b46ff2b
Fernando Rocha <fernando.rocha@im.ufal.br>
2023-06-08 19:24:23 -0300

    feat: commit inicial

---
+++ code/src/components/BestTransport.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="title">
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand class="ml-2">
+        <b>{{ appName }}</b>
+      </b-navbar-brand>
+    </b-navbar>
+  </div>
+
+</template>
+
+<script>
+import {
+  BNavbar,
+  BNavbarBrand,
+} from 'bootstrap-vue'
+
+export default {
+  components: {
+    BNavbar,
+    BNavbarBrand,
+  },
+  data() {
+    const appName = ''
+
+    return {
+      appName,
+    }
+  },
+  created() {
+    // Implemente aqui o GET dos dados da API REST
+    // para que isso ocorra na inicialização da pagina
+    this.appName = 'Melhor Frete'
+  },
+  methods: {
+    // Implemente aqui os metodos utilizados na pagina
+    methodFoo() {
+      console.log(this.appName)
+    },
+  },
+}
+</script>
+
+<style scoped>
+.title .navbar {
+  background-color: #00aca6 !important;
+}
+
+.title .navbar-brand {
+  margin-left: 20px;
+}
+</style>
